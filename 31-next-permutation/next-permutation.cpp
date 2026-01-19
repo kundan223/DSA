e27a3219869ea6480df2eb4baa5df7bfe1a57676
Kundan Yadav <135807600+kundan223@users.noreply.github.com>
2026-01-19 14:45:55 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (91.71%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n - 2;
 
+        int i = n - 2;
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
 
         if (i >= 0) {
             int j = n - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
+            while (nums[j] <= nums[i]) {
                 j--;
             }
             swap(nums[i], nums[j]);
