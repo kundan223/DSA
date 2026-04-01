8dfaf7bb9710b49bfd20baeefa0784358c622818
Kundan Yadav <135807600+kundan223@users.noreply.github.com>
2026-04-01 19:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (21.55%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+        
+        // Use standard library constants
+        const long long MAX_INT = INT_MAX;
+        const long long MIN_INT = INT_MIN;
+        
         int i = 0;
         int n = s.length();
         
-      
+        // Step 1: Skip leading whitespace
         while (i < n && s[i] == ' ') {
             i++;
         }
         
-        string num = "";  
-        int count = 0;     
-        
-
-        if (i < n && (s[i] == '-' || s[i] == '+')) {
-            num.push_back(s[i]);
-            i++;
+        // Check if we've reached the end
+        if (i == n) {
+            return 0;
         }
         
-
-        while (i < n && isdigit(s[i])) {
-            num.push_back(s[i]);
-            count++;
-            i++;
-        }
-        if (count == 0) return 0;
-
-        long long result = 0;
+        // Step 2: Check for sign
         int sign = 1;
-        int start = 0;
-        
-        if (num[0] == '-') {
+        if (s[i] == '+') {
+            i++;
+        } else if (s[i] == '-') {
             sign = -1;
-            start = 1;
-        } 
-        else if (num[0] == '+') {
-            start = 1;
+            i++;
         }
         
-        for (int j = start; j < num.length(); j++) {
-            result = result * 10 + (num[j] - '0');
+        // Step 3: Read digits and convert
+        long long res = 0;
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            res = res * 10 + digit;
             
-
-            if (sign == 1 && result > INT_MAX)
-                return INT_MAX;
-            if (sign == -1 && -result < INT_MIN)
+            if (sign * res <= INT_MIN) {
                 return INT_MIN;
+            }
+            if (sign * res >= INT_MAX) {
+                return INT_MAX;
+            }
+            
+            i++;
         }
         
-        return sign * result;
+        // Step 4: Apply sign and return
+        return static_cast<int>(res * sign);     
     }
 };
