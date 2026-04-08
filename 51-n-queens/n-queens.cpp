d987c640269745f5e0e8e0d67af9449aa2909930
Kundan Yadav <135807600+kundan223@users.noreply.github.com>
2026-04-08 23:42:58 +0530

    Time: 3 ms (62.30%) | Memory: 10.3 MB (54.92%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -6,7 +6,7 @@ public:
                vector<int>& diag1,
                vector<int>& diag2) {
 
-        // ✅ Base case: all rows filled
+    
         if (row == n) {
             ans.push_back(board);
             return;
@@ -14,21 +14,18 @@ public:
 
         for (int col = 0; col < n; col++) {
 
-            // ✅ Check if safe in O(1)
             if (colUsed[col] == 0 &&
                 diag1[row + col] == 0 &&
                 diag2[n - 1 + col - row] == 0) {
 
-                // 👉 Place queen
                 board[row][col] = 'Q';
                 colUsed[col] = 1;
                 diag1[row + col] = 1;
                 diag2[n - 1 + col - row] = 1;
 
-                // 👉 Go to next row
                 solve(row + 1, n, board, ans, colUsed, diag1, diag2);
 
-                // 🔁 Backtrack (UNDO)
+
                 board[row][col] = '.';
                 colUsed[col] = 0;
                 diag1[row + col] = 0;
@@ -43,8 +40,8 @@ public:
         vector<string> board(n, string(n, '.'));
 
         vector<int> colUsed(n, 0);
-        vector<int> diag1(2*n - 1, 0);  // row+col
-        vector<int> diag2(2*n - 1, 0);  // n-1 + col-row
+        vector<int> diag1(2*n - 1, 0);  
+        vector<int> diag2(2*n - 1, 0);  
 
         solve(0, n, board, ans, colUsed, diag1, diag2);
 
