b076f2b74c7779bb9b8eb248f7b525e70a07583c
Kundan Yadav <135807600+kundan223@users.noreply.github.com>
2026-04-10 19:59:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.53%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-
-    double helper(double x, long long n) {
-        if (n == 0) return 1;
-
-        if (n % 2 == 1) {
-            return x * helper(x, n - 1);
-        } 
-        else {
-            double half = helper(x, n / 2);
-            return half * half;
-        }
-    }
-
     double myPow(double x, int n) {
-        long long N = n;   // ✅ FIX
+        long long  N = n ;
+        double ans  = 1 ;
+        
+        if ( N < 0 ){
+            N = -N;
+            x = 1/x;
+        };
 
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;        // ✅ SAFE NOW
+        while ( N > 0 ){
+            if ( N & 1 ){
+                ans *= x ;
+            }
+            x *= x ;
+            N >>= 1 ;
         }
 
-        return helper(x, N);
+        return ans ;
     }
 };
