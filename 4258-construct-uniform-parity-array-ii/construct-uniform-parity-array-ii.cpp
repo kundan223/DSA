de836a4ac81eafaec4dc850e65b2869a724dec11
Kundan Yadav <135807600+kundan223@users.noreply.github.com>
2026-09-03 11:15:40 +0530

    Time: 26 ms (34.56%) | Memory: 173.9 MB (10.21%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+
+    bool try_even(vector<int>& nums1, int size, vector<int>& parity) {
+
+        int min_odd = INT_MAX;
+
+        for (int i = 0; i < size; i++) {
+            if (nums1[i] % 2 == 1) {
+                min_odd = min(min_odd, nums1[i]);
+            }
+        }
+
+        // No odd numbers means everything is already even
+        if (min_odd == INT_MAX) {
+            return true;
+        }
+
+        // The smallest odd number itself cannot become
+        // a positive even number
+        for (int i = 0; i < size; i++) {
+
+            if (nums1[i] % 2 == 1) {
+
+                if (nums1[i] == min_odd) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool try_odd(vector<int>& nums1, int size, vector<int>& parity) {
+
+        int min_odd = INT_MAX;
+
+        for (int i = 0; i < size; i++) {
+            if (nums1[i] % 2 == 1) {
+                min_odd = min(min_odd, nums1[i]);
+            }
+        }
+
+        // No odd number means we cannot make even numbers odd
+        if (min_odd == INT_MAX) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+
+            if (nums1[i] % 2 == 0) {
+
+                if (nums1[i] - min_odd < 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool uniformArray(vector<int>& nums1) {
+
+        int s = nums1.size();
+
+        vector<int> parity(s, 0);
+
+        int odd = 0;
+        int even = 0;
+
+        for (int i = 0; i < s; i++) {
+
+            if (nums1[i] % 2 == 1) {
+                parity[i] = 1;
+                odd++;
+            }
+            else {
+                parity[i] = 0;
+                even++;
+            }
+        }
+
+        bool makeEven = try_even(nums1, s, parity);
+        bool makeOdd = try_odd(nums1, s, parity);
+
+        return makeEven || makeOdd;
+    }
+};
